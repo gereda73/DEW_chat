7a0dc3e42d77e375376ce5978c15dd1b9bd594e2
jose gereda <gereda73@gmail.com>
2012-01-30 22:35:46 -0500

    Inicio Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DEWChat::Application.config.secret_token = '47359df567ad45a5cf68e7fea2a4cfbacfacf12c901e2f1f4cf482cde4d9bc54559830cb977a3ae4df6ad424e905d029f14e8958f65c89d9437a58f631e52e94'
